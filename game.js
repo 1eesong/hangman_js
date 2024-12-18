bdb5e316a623d974305d01bc3d541e3c54f9c979
1eesong <hui161@naver.com>
2024-12-18 10:07:04 +0900

    third commit

--- game.js
+++ game.js
@@ -1,125 +1,123 @@
 import {ANSWERS, TIME_LIMIT} from "constants.js";
 import {makeVisible, makeinVisible} from "utils.js";
 
-
 class Game {
     #timerCount;
-    #timerId;
+    #timerId; // ???
 
     // 정답 여부
     #currentStage;
     #answer;
-    #problemText;
+    #problem;
 
-    canvas = document.getElementsByClassName("canvas");
+    hangman = document.getElementsByClassName(".canvas > img");
     timer = document.getElementById("timer");
-    problem = document.getElementById("problemText");
+    problemText = document.getElementById("problemText");
     answerText = document.getElementById("answerText");
     alphabetButton = document.querySelectorAll(".alphabets > button");
     alphabetEventListeners =[];
 
     constructor() {
-        // 게임 시작
-        this.#timeCount = TIME_LIMIT;
+        // 게임 시작 세팅
+        this.#timerCount = TIME_LIMIT;
         this.#currentStage = 0;
         this.#answer = ANSWERS[Math.floor(math.random() * ANSWERS.length)]; // array[Math.floor(Math.random()*array.length)] 난수 생성
-        this.#problemText = this.#answer
-        
-        // 버튼 입력
-        this.alphabetButton.addEventListener("click", {
+        this.#problem = this.#answer
+            .split("")
+            .reduce((acc, cur) => (cur === " "? acc + " " : acc + "_"), ""); // ???
 
-        } )
+        this.timer.innerText = this.#timerCount;
+        this.problemText.innerText = this.#problem
+        makeVisible(this.problemText);
+        this.answerText.innerText = `정답 : ${this.#answer}`;
+        makeinVisible(this.answerText);
 
+        // 게임 결과 (promise) => ??? 복습 
+        return new Promise((resolve) => {
+            // 타이머 0초 -> 게임오버 -> promise resolve(false)
+            this.#timerId = setInterval(() => {
+                if(this.#timerCount <= 0) resolve(false);
+                else this.#decreaseTimerCount();
+            }, 1000);
 
-
-        // 정답 여부 확인 (promise)
-        const promise = new Promise((resolve, reject) => {
-            if() {
-                resolve()
-            } else if(this.#timerCount <= 0){
-                
-                
-            } else {
-                reject()
-            }
-        })
-        
-    
-
-        
-
-
+            // 알파벳 버튼 => ???
+            this.alphabetButton.forEach((element) => {
+                const onClick = () => {
+                    this.#clickAlphabet(element, resolve);
+                    element.removeEventListener("click", onClick);
+                }
+                this.alphabetEventListeners.push(onClick);
+                element.addEventListener("click", onClick);
+            })
+        }).then(this.#gameOver);
     }
+        
+    // 알파벳 버튼 실행
+    #clickAlphabet = (element, resolve) => {
+        // 버튼 클릭 시 비활성화
+        element.classList.add("button-invisible");
+        element.removeEventListener("click", () =>
+        this.#clickAlphabet(element, resolve)
+        ); 
 
-    // 타이머 감소
-    #decreaseTimeCount() {
-        this.#timeCount--;
-        this.timer.innerText = this.#timeCount;
-    }
-
-
-    // 버튼 사라짐
-    #clickAlphabet(element) {
-        element.classList.add("")
-        element.removeEventListener("click", this.#clickAlphabet(element))
-    }
-    
-
-
-
-
-
-    if(this. === this.#answer)
-
-
-
-        // 성공
-        if(this.#problemText === this.#answer) {
-
+        // 알파벳 정답 여부 확인 => ??? 복습
+        if(this,#answer.includes(element.innerText)) {
+            this.#updateProblem(element.innerText);
+            // 전체 정답 -> 게임오버 -> promise resolve(true)
+            if(this.#problem === this.#answer) resolve(true);
+        } else {
+            // 행맨 미완성 -> 다음 stage
+            if(this.#currentStage < this.hangman.length - 1)
+                this.#nextStage();
+            // 행맨 완성 -> 게임 오버 -> promis resolve(false)
+            else resolve(false);
         }
+    }
 
+    // 타이머 1초씩 감소
+    #decreaseTimerCount() {
+        this.#timerCount--;
+        this.timer.innerText = this.#timerCount;
+    }
 
-        // 다음 stage
-        currentStage
+    // stage 1 증가 + 행맨 증가 => ???
+    #nextStage() {
+        this.#currentStage++;
+        Array.from(this.hangman)
+            .slice(0, this.#currentStage)
+            .forEach((element) => element.classList.remove("invisible"));
+    }
 
+    // 알파벳 입력 시 problem 업데이트 => ???
+    #updateProblem(newAlphabet) {
+        this.#problem - this.#problem
+            .split("")
+            .reduce((acc, cur, index) => {
+                if(cur === " " || cur !== "_") 
+                    return acc + cur;
+                if(this.#answer[index] === newAlphabet) 
+                    return acc + newAlphabet;
+                else return acc + "_";
+            })
+    }
 
-        // 게임오버
-
-
-
-    //
-
-
-
-
-
-
-
-
-
+    // 실패 -> 게임오버 false / 성공 -> 게임오버 true => ???
+    #gameOver = (result) => {
+        // 타이머 삭제
+        clearInterval(this.#timerId);
+        // 정답
+        makeVisible(this.answerText);
+        // 초기화
+        this.hangman.forEach((element) => 
+            element.classList.add("invisible")
+        );
+        makeInvisible(this.problemText);
+        this.alphabetButton.forEach((element, index) => {
+            element.classList.remove("button-invisible");
+            element.removeEventListener("click", this.alphabetEventListeners[index]);
+        })
+        return result;
+    }
 }
 
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
+export default Game;
--- system.js
+++ system.js
@@ -1,64 +1,68 @@
 import {ATTEMPTS} from "constants.js";
 import {makeVisible, makeinVisible} from "utils.js";
+import Game from "game.js";
 
 class System {
+    #count;
 
 const counter = document.getElementById("counter");
-const timer = document.getElementById("timer");
 const startText = document.getElementById("startText");
 const successText = document.getElementById("successText");
 const failText = document.getElementById("failText");
 const overText = document.getElementById("overText");
 const gameStart-button = document.getElementById("gameStart-button");
 const restart-button = document.getElementById("restart-button");
-const reset-button = document.getElementById("reset-button");
 
     constructor() {
-        
-        #count;
-        #time;
-
+        // 시스템 시작 세팅
         this.#count = ATTEMPTS;
-        counter.innerText = this.#count;
 
-        this.#time = TIME_LIMIT;
-        timer.innerText = this.#time; 
+        this.counter.innerText = this.#count;
+        makeVisible(this.startText);
+        this.gameStart-button.addEventListener("click", this.#start);
+        this.restart-button.addEventListener("click", this.#restart);
 
-        // 시도 횟수 1회 차감
+    }
+        
+    // 시작하기 버튼 -> 시도 횟수 1회 차감
+    #decreaseCount() {
+        this.#count--;
+        this.counter.innerText = this.#count;
+    }
 
+    // 시도 횟수 남음 -> 다시 시작
+    #gameStart = async () -> {
+        this.#decreaseCount();
+        const result = await new Game();
 
-        // 시작하기 버튼
-        #start() {
-            makeinVisible(startText)
+        if(result) {
+            // 성공 -> 텍스트
+            makeVisible(this.successText);
+        } else {
+            // 실패 -> 텍스트
+            if(this.#count > 0) {
+                makeVisible(this.failText);
+            } else {
+                // 게임오버 -> 텍스트
+                makeVisible(this.overText);
+            }
         }
-        
-        gameStart-button.addEventListener("click", this.#start)
-        
-        // 시도 횟수 1회 차감
+    };
 
+    // 다시 시작 버튼
+    #restart() {
+        makeinVisible(this.failText);
+        this.#gameStart();
+    }
 
-        // 다시 시작 버튼
+    // 시스템 초기화
+    cleanup() {
+        this.counter.innerText = ATTEMPTS;
 
-
-
-
-        // 성공
-
-
-
-        // 실패
-
-
-
-        // 게임오버
-
-        // 처음부터 다시하기 버튼
-        reset-button.addEventListener("click", this.)
-
-
-        // 시스템 초기화
-
-
+        this.gameStart-button.removeEventListener("click", this.#start);
+        this.restart-button.removeEventListener("click", this.#restart);
+        makeInvisible(this.successText);
+        makeInvisible(this.overText);
     }
 }
 
